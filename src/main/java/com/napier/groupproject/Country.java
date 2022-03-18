c4bfc31ce50034a5f0e7d7b005d2b0cd8be5be26
BrandonStreets-Mcleod <brandon.streetsmcleod@gmail.com>
2022-03-18 15:32:48 +0000

    Updated GitHub Actions to run integration tests and compilation separately

--- src/main/java/com/napier/groupproject/Country.java
+++ src/main/java/com/napier/groupproject/Country.java
@@ -20,3 +20,5 @@ public class Country {
     public int capital;
     public int code2;
 }
+
+
---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+import com.napier.groupproject.App;
+import com.napier.groupproject.Country;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        assertNotNull(app.allCapitalCitiesInContinent("Asia"));
+
+    }
+}
