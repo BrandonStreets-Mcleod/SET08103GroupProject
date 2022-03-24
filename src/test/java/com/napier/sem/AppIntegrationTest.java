07625ecf5a3523760b2670f9f52687e974ed5fc8
BrandonStreets-Mcleod <brandon.streetsmcleod@gmail.com>
2022-03-24 16:34:00 +0000

    Added more integration tests

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.napier.sem;
 
 import com.napier.groupproject.App;
+import com.napier.groupproject.City;
 import com.napier.groupproject.Country;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -24,5 +25,19 @@ public class AppIntegrationTest
     {
         Country country = app.populationOfCountries().get(1);
         assertEquals(country.population, 1013662000);
+    }
+
+    @Test
+    void testGetCity()
+    {
+        City city = app.allCities().get(1);
+        assertEquals(city.Name, "Mumbai (Bombay)");
+    }
+
+    @Test
+    void testGetCapitalCity()
+    {
+        City city = app.allCapitalCities().get(1);
+        assertEquals(city.Name, "Seoul");
     }
 }
