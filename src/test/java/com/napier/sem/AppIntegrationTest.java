068cd66941a212c7257a1b00aaba1e45bb746ff8
BrandonStreets-Mcleod <brandon.streetsmcleod@gmail.com>
2022-04-06 10:02:36 +0100

    Added error message to assertEquals found by PMD

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -24,76 +24,76 @@ public class AppIntegrationTest
     void testGetCountry()
     {
         Country country = app.populationOfCountries().get(1);
-        assertEquals(country.population, 1013662000);
+        assertEquals(country.population, 1013662000, "Population does not equal 1013662000");
     }
 
     @Test
     void testGetCountryInContinent()
     {
         Country country = app.populationOfCountriesInContinent("Asia").get(0);
-        assertEquals(country.name, "China");
+        assertEquals(country.name, "China", "Country name does not equal China");
     }
 
     @Test
     void testGetCountryInRegion()
     {
         Country country = app.populationOfCountriesInRegion("North America").get(0);
-        assertEquals(country.name, "United States");
+        assertEquals(country.name, "United States", "Country name does not equal United States");
     }
 
     @Test
     void testGetCity()
     {
         City city = app.allCities().get(1);
-        assertEquals(city.Name, "Seoul");
+        assertEquals(city.Name, "Seoul", "City does not equal Seoul");
     }
 
     @Test
     void testGetCityInContinent()
     {
         City city = app.allCitiesInAContinent("Asia").get(0);
-        assertEquals(city.Name, "Mumbai (Bombay)");
+        assertEquals(city.Name, "Mumbai (Bombay)","City does not equal Mumbai (Bombay)");
     }
 
     @Test
     void testGetCityInRegion()
     {
         City city = app.allCitiesInARegion("North America").get(0);
-        assertEquals(city.Name, "New York");
+        assertEquals(city.Name, "New York","City does not equal New York");
     }
 
     @Test
     void testGetCityInCountry()
     {
         City city = app.allCitiesInACountry("United States").get(0);
-        assertEquals(city.Name, "New York");
+        assertEquals(city.Name, "New York","City does not equal New York");
     }
 
     @Test
     void testGetCityInDistrict()
     {
         City city = app.allCitiesInADistrict("California").get(0);
-        assertEquals(city.Name, "Los Angeles");
+        assertEquals(city.Name, "Los Angeles", "City does not equal Los Angeles");
     }
 
     @Test
     void testGetCapitalCity()
     {
         City city = app.allCapitalCities().get(1);
-        assertEquals(city.Name, "Jakarta");
+        assertEquals(city.Name, "Jakarta", "City does not equal Jakarta");
     }
 
     @Test
     void testGetCapitalCityInContinent()
     {
         City city = app.allCapitalCitiesInContinent("Asia").get(1);
-        assertEquals(city.Name, "Jakarta");
+        assertEquals(city.Name, "Jakarta", "City does not equal Jakarta");
     }
 
     @Test
     void testGetCapitalCityInRegion()
     {
         City city = app.allCapitalCitiesInRegion("North America").get(0);
-        assertEquals(city.Name, "Washington");
+        assertEquals(city.Name, "Washington", "City does not equal Washington");
     }
 }
