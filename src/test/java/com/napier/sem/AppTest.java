2aafdc8368429b22f8011a36954ecd27d9df6695
BrandonStreets-Mcleod <brandon.streetsmcleod@gmail.com>
2022-04-06 09:51:37 +0100

    Removed unused imports found by PMD

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -5,11 +5,8 @@ import com.napier.groupproject.City;
 import com.napier.groupproject.Country;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest
 {
